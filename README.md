45988dbd1526635d1ce62110fb5f78a0d5349038
a092devs <arsalanakhtar0@gmail.com>
2023-01-01 11:23:43 +0530

    Day 1 - Working with Variables in Python to Manage Data

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1 align="center">100 Days of Code: The Complete Python Pro Bootcamp
+</h1>
+
+## 🔰 Beginner 
+- [Day 1:](https://github.com/a092devs/100-days-of-python/tree/master/day01) Working with Variables in Python to Manage Data
+
+
+## ⚙ Tools and Technologies Covered
+- Python 3
+- VSCode
+- Git, GitHub and Version Control
---
+++ day01/band_name_generator.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+#2. Ask the user for the city that they grew up in.
+#3. Ask the user for the name of a pet.
+#4. Combine the name of their city and pet and show them their band name.
+#5. Make sure the input cursor shows on a new line:
+
+print("Welcome to the band name geneator!")
+city = input("What's the name of the city?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
